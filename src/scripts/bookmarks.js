307adc172b969ff9850098777efae2d453662aee
Rachel Reilly <r.a.reill18@gmail.com>
2020-08-19 18:54:20 -0500

    adding static html for main page

--- src/scripts/bookmarks.js
+++ src/scripts/bookmarks.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import store from './store';
 
 
+//check html and merge css
+//add stars
 
 
 
@@ -23,9 +25,19 @@ const generateAddButton = function () {
 };
 
 const generateFilterButton = function () {
-    const filter = '<button type="button" class="button filter">Filter</button><ul class="hidden"><li>*</li><li>**</li><li>***</li><li>****</li><li>*****</li></ul>';
+    const filter = `<button type="button" class="button filter">Filter</button>
+    <ul class="hidden">
+    <li><span>☆</span></li>
+    <li><span>☆</span><span>☆</span></li>
+    <li><span>☆</span><span>☆</span><span>☆</span></li>
+    <li><span>☆</span><span>☆</span><span>☆</span><span>☆</span></li>
+    <li><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></li></ul>`;
     return filter;
 };
+
+/*const getRating = function(){
+    const rating = store.store.
+}*/
 
 
 
@@ -33,18 +45,30 @@ const generateFilterButton = function () {
 //when hidden is removed change flex of place
 const generateBookmarks = function () {
     const bookmarks = [];
+    //make function to make star rating variable, not static
+
     const bookmarkHtml = store.store.bookmarks.forEach(bookmark => {
-        let html = `<section><div><h3>${bookmark.title}</h3><span>${bookmark.rating}</span></div>
-    <div class='hidden'>
-        <a href = ${bookmark.link}>
-            <button class = 'visit' type = 'button'>Visit site</button>
-        </a>
-        <div class='description'><p>${bookmark.description}</p></div>
-    </div></section>`;
+        let html = `
+        <section class = "bookmark-section">
+            <div class = 'collapsed bookmark'>
+            <h3>${bookmark.title}</h3>
+                <div class="rating">
+                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                </div>
+            </div>
+            
+            <div class= "hidden bookmark-full">
+                <a href = ${bookmark.link}>
+                <button class = 'visit' type = 'button'>Visit site</button>
+                </a>
+            <div class='description'><p>${bookmark.description}</p></div>
+        </div>
+        </section>`;
+
         bookmarks.push(html);
 
     });
-   const htmlString = bookmarks.join(',');
+   const htmlString = bookmarks.join('');
    const html = `<main>${htmlString}</main>`;
    return html;
 
@@ -78,7 +102,41 @@ const render = (str => {
 });
 
 
+
+
+
+
+const clickToggleOpen = function(){
+    //get section tag and toggle hidden on 
+    //class of div beneath title and rating
+
+
+}
+
+//on clicking puls button, render whole new page to the main
+//taking over the whole view except
+//change background color??
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const bindEventListeners = function (){
+
+};
+
+
 export default {
     generateListView,
-    render
+    render,
+    bindEventListeners
 };
